e5fb65350116018840a5f7a09a7024de0ce5accd
John Jefferson Bautista <jeppbautista@gmail.com>
2019-09-07 14:18:58 +0800

    Final final 2

--- pxbot/pxbot/urls.py
+++ pxbot/pxbot/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
-urlpatterns = [
+
+urlpatterns = {
     path('admin/', admin.site.urls),
     path('dashboard/', include('dashboard.urls')),
-]
+    re_path('^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+}
